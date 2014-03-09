7a726ae3e3471d895517fb78ca13bd6a3825ab67
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-03-09 17:33:54 +0400

    config

--- lib/the_notification.rb
+++ lib/the_notification.rb
@@ -1,4 +1,5 @@
 require "the_notification/version"
+require 'the_notification/config'
 
 module TheNotification
   class Engine < Rails::Engine; end
---
+++ lib/the_notification/config.rb
@@ -0,0 +1,20 @@
+module TheNotification
+  def self.configure(&block)
+    yield @config ||= TheStorages::Configuration.new
+  end
+
+  def self.config
+    @config
+  end
+
+  # Configuration class
+  class Configuration
+    include ActiveSupport::Configurable
+
+    config_accessor :notifications_type
+  end
+
+  configure do |config|
+    config.notifications_type = :html # :json
+  end
+end
